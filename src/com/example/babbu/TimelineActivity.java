0c15ec4fa2cca5558a6157c89649295c811d4098
sukhmeet <sukhmeet2390@gmail.com>
2013-09-11 07:53:20 +0530

    Added SimpleAdapter and custom layout using RelativeLayout, do direct mapping of rows in this layout using simpleCursorAdapter

--- src/com/example/babbu/TimelineActivity.java
+++ src/com/example/babbu/TimelineActivity.java
@@ -3,6 +3,8 @@ package com.example.babbu;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 /**
@@ -13,23 +15,30 @@ import android.widget.TextView;
  * To change this template use File | Settings | File Templates.
  */
 public class TimelineActivity extends Activity {
-    TextView textView;
+    static final String[] FROM = {StatusData.COL_USER, StatusData.COL_CREATED_AT, StatusData.COL_STATUS_TEXT};
+    static final int[] TO = {R.id.text_user, R.id.text_time, R.id.text_status};
+    ListView listView;
     StatusData statusData;
     Cursor cursor;
+    SimpleCursorAdapter simpleCursorAdapter;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timeline);
 
-        textView = (TextView) findViewById(R.id.status_data);
+        listView = (ListView) findViewById(R.id.list);
 
         cursor = ((BabbuApp) getApplication()).statusData.query();
 
-        while (cursor.moveToNext()){
-            String user = cursor.getString(cursor.getColumnIndex(StatusData.COL_USER));
-            String status = cursor.getString(cursor.getColumnIndex(StatusData.COL_STATUS_TEXT));
-            textView.append(String.format("%s : %s\n",user, status));
-        }
+        simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.row, cursor,FROM, TO  );
+        listView.setAdapter(simpleCursorAdapter);
+//        while (cursor.moveToNext()){
+//            String user = cursor.getString(cursor.getColumnIndex(StatusData.COL_USER));
+//            String status = cursor.getString(cursor.getColumnIndex(StatusData.COL_STATUS_TEXT));
+//            textView.append(String.format("%s : %s\n",user, status));
+//        }
+
     }
 
 }
